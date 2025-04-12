e70837deff1cea0badfc32f1b48132c1c4a4aabf
dxbbpi <olafz9zzz@gmail.com>
2025-04-13 04:11:55 +0700

    Add files via upload

---
+++ membersrp.js
@@ -0,0 +1,6 @@
+const members = [
+    { "name": "Test 1 ", "fbId": "Test" },
+
+];
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.addEventListener('keydown', e => { if (e.keyCode == 123) e.preventDefault(); });
