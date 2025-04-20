9b7c40f9d8c7627a208debcdb592e02b6a9ee54d
dxbbpi <olafz9zzz@gmail.com>
2025-04-20 17:50:04 +0700

    Delete members.js

--- members.js
+++
@@ -1,6 +0,0 @@
-const members = [
-    { "name": "test 2", "fbId": "test 2" },
-
-];
-document.addEventListener('contextmenu', e => e.preventDefault());
-document.addEventListener('keydown', e => { if (e.keyCode == 123) e.preventDefault(); });
